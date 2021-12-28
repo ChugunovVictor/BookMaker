1d2f3dacb1976044396149f373d756dff3639500
victor.chugunov <Portal112358!>
2021-12-28 23:55:19 +0400

    redesign

--- src/main/java/org/chugunov/ui/controllers/ProcessTabController.java
+++ src/main/java/org/chugunov/ui/controllers/ProcessTabController.java
@@ -1,19 +1,23 @@
 package org.chugunov.ui.controllers;
 
+import javafx.application.Platform;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TitledPane;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
 import org.chugunov.ui.Controller;
+import org.jsoup.internal.StringUtil;
 
 import java.net.URL;
-import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class ProcessTabController implements Initializable, Controller {
   @FXML
-  private TitledPane settings, content, basic;
+  private Accordion accordion;
+  @FXML
+  private TitledPane preview, content, basic;
 
   // Names of controller variables should be <fx:id>Controller
   @FXML
@@ -23,43 +27,49 @@ public class ProcessTabController implements Initializable, Controller {
   @FXML
   private BasicController basicPageController;
 
+  private TitledPane active;
+
   private org.chugunov.model.Process process = new org.chugunov.model.Process();
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-    settings.setExpanded(false);
-    content.setExpanded(false);
-
-    basic.setPrefHeight(1000);
-    settings.setPrefHeight(10);
-    content.setPrefHeight(10);
-
-    VBox.setVgrow(basic, Priority.ALWAYS);
-    VBox.setVgrow(settings, Priority.NEVER);
-    VBox.setVgrow(content, Priority.NEVER);
-
-    addExpandListener(basic, settings, content);
-    addExpandListener(settings, basic, content);
-    addExpandListener(content, basic, settings);
   }
 
-  private void addExpandListener(TitledPane subject, TitledPane... rest) {
-    subject.setPrefHeight(1000);
-    VBox.setVgrow(subject, Priority.ALWAYS);
-    subject.expandedProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue) {
-        Arrays.stream(rest).forEach(c -> {
-          c.setExpanded(false);
-          c.setPrefHeight(10);
-          VBox.setVgrow(c, Priority.NEVER);
-        });
-      }
-    });
+  private void showAlert(String message) {
+    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    alert.setResizable(false);
+    alert.setTitle("Attention");
+    alert.setHeaderText(null);
+    alert.setContentText("Not enough data:\n" + message);
+    alert.showAndWait();
   }
 
   public void init() {
     this.basicPageController.init(process);
     this.contentPageController.init(process);
     this.previewPageController.init(process);
+
+    active = basic;
+
+    accordion.expandedPaneProperty().addListener((ObservableValue<? extends TitledPane> observable, TitledPane oldPane, TitledPane newPane) -> {
+      boolean expand = accordion.getPanes().stream().map(c -> c.isExpanded()).reduce((a, c) -> a || c).orElse(true);
+      boolean contentCheck = newPane == content && StringUtil.isBlank(process.getBasic().getSite());
+      boolean previewCheck = newPane == preview && (
+          StringUtil.isBlank(process.getContent().getAddressToStart())
+              || StringUtil.isBlank(process.getContent().getSelectorContent())
+              || StringUtil.isBlank(process.getContent().getSelectorNavigationNext())
+              || StringUtil.isBlank(process.getContent().getSelectorTitle())
+      );
+
+      if (!expand || contentCheck || previewCheck) {
+        if (contentCheck) showAlert("Please fill the URL field");
+        if (previewCheck) showAlert("Please fill selectors fields and start page");
+        Platform.runLater(() -> {
+          accordion.setExpandedPane(active);
+        });
+      } else {
+        if (newPane != null) active = newPane;
+      }
+    });
   }
 }
